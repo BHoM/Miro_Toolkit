0c89d5df53eb7a617f37f62ab2930385df0a0da5
Michael Hoehn <michael.hoehn@burohappold.com>
2026-05-14 15:43:17 -0400

    Toolkit setup

--- Miro_Adapter/CRUD/Create/_ICreate.cs
+++ Miro_Adapter/CRUD/Create/_ICreate.cs
@@ -28,9 +28,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BH.Adapter.SoftwareName
+namespace BH.Adapter.Miro
 {
-    public partial class SoftwareNameAdapter : BHoMAdapter
+    public partial class MiroAdapter : BHoMAdapter
     {
         // NOTE: CRUD folder methods
         // All methods in the CRUD folder are used as "back-end" methods by the Adapter itself.
